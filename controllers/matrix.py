3a86835933e4026df072872288271767b168bb9c
geoffrey <geoffrey.spaur@gmail.com>
2021-08-08 16:16:21 +0200

    -

--- controllers/matrix.py
+++ controllers/matrix.py
@@ -2,6 +2,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import permissions
 import json
+# import sys
+# from remotematrixapi.scripts_uart_max7219 import *
+
+
+
 
 
 @api_view(['GET', 'POST'])
@@ -16,7 +21,14 @@ def state(request):
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 matrix[i][j] = int(matrix[i][j])
-        array = []
-        for line in matrix:
-            array += line
-        return Response({ 'matrix': matrix, 'line': array })
+        # jorisclass.sendmatrix(matrix)
+        return Response({ 'matrix': matrix })
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def reset(request):
+    if request.method == 'POST':
+        # uart_rpi = uart_max7219_ctrl_class(baudrate = 230400)
+        # uart_rpi.init_static_ram()
+        # uart_rpi.close_uart()
+        return Response({ 'msg': 'success' })
--- urls.py
+++ urls.py
@@ -5,5 +5,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('ping', ping.getPing),
     path('matrix/state', matrix.state)
+    path('matrix/reset', matrix.reset)
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
