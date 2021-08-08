853049ec1a392898c90fae4df55caf4e981a3409
geoffrey <geoffrey.spaur@gmail.com>
2021-08-08 16:17:30 +0200

    -

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('ping', ping.getPing),
-    path('matrix/state', matrix.state)
+    path('matrix/state', matrix.state),
     path('matrix/reset', matrix.reset)
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
